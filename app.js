2ef6b9665e5913ed8cd89cb6ef2149c2624ebc1e
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-02 16:54:41 -0600

    commented instructions

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// create an array of objects that use the following schema: {item: string, cost: number, quantity: number}
+const groceries = [];
+// tax rate as a constant
+const TAX_RATE = 0.07;
+
+// create a varable for the subtotal
+
+// loop through the groceries
+// calculate the amount for each item
+// add the value to the subtotal
+
+// calculate the total
+
+// log the subtotal as well as the total to the console. but only display the values to 2 decimal points
